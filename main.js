7d88fe647be4d9a0900301247907d00e24af9dec
Kevin Oluoch <koluoch28@gmail.com>
2024-12-31 15:39:07 +0300

    This is the complete project for the Rock-Paper-Scissor played on browser console.

--- main.js
+++ main.js
@@ -1,66 +1,68 @@
-// STEPS
-// Variables for tracking scores
-const humanScore = 0;
-const computerScore = 0;
-// 1. Create a function for computer play: Use random selection
-
-function computerPlay () {
+// // 1. Create a computer selection using random selection
+function getComputerChoice() {
     const random = Math.floor(Math.random() * 3);
-
-    // Add if statements to check on the value of the random
-        // There are three choices of the random (0, 1 & 2)
     if (random === 0) {
-        console.log("rock");
+        return "rock";
     }else if (random === 1) {
-        console.log("paper");
-    }else {
-        console.log("scissors");
+        return "paper";
+    }else{
+        return "scissors";
+    };
+};
+// // 2. Create human selection function using prompt
+function getHumanChoice() {
+    const choice = prompt("Enter Rock, Paper or Scissor")
+    const str = choice.toLowerCase();
+    return str;
+};
+// // Score variables
+let humanScore = 0;
+let computerScore = 0;
+// Function to play the game
+function playGame() {
+     for (let i = 1; i <= 5; i++) {
+        computerSelection = getComputerChoice();
+        humanSelection = getHumanChoice();
+        playRound(computerSelection, humanSelection);
+     };
+};
+playGame();
+
+// //3. Create a function to play one round of the game
+function playRound() {
+    if (computerSelection === humanSelection) {
+        console.log("It is a Draw, play again!");
+        computerScore;
+        humanScore;
+    } else if (computerSelection === "rock" && humanSelection === "paper"){
+        console.log("Paper wraps Rock! Player Wins.");
+        humanScore++;
+    } else if (computerSelection === "paper" && humanSelection === "scissors") {
+        console.log("Scissors cuts Paper! Player Wins.");
+        humanScore++;
+    }else if (computerSelection === "scissors" && humanSelection === "rock") {
+        console.log("Rock beats Scissors! Player Wins.");
+        humanScore++;
+    }else if (computerSelection === "rock" && humanSelection === "scissors") {
+        console.log("Rock beats Scissors! Computer Wins.");
+        computerScore++;
+    }else if (computerSelection === "paper" && humanSelection === "rock") {
+        console.log("Paper wraps Rock! Computer Wins.");
+        computerScore++;
+    }else if (computerSelection === "scissors" && humanSelection === "paper") {
+        console.log("Scissors cuts Paper! Computer Wins.");
+        computerScore++;
+    };
+
+    // Check for the winner or lose or draw;
+    if (`${humanScore}` > `${computerScore}`) {
+        console.log(`Your scores are:\nPlayer Score: ${humanScore}.\nComputer Score: ${computerScore}.`);
+        console.log("Yay! Player Wins");
+    } else if(`${humanScore}` < `${computerScore}`){
+        console.log(`Your scores are:\nPlayer Score: ${humanScore}.\nComputer Score: ${computerScore}.`);
+        console.log("Sorry! Computer Wins");
+    } else {
+        console.log(`Your scores are:\nPlayer Score: ${humanScore}.\nComputer Score: ${computerScore}.`);
+        console.log("It is a draw. Play again");
     }
-}
-
-//2. Create a function for Human play
-
-function humanPlay() {
-    // create a prompt for entering human choice
-
-    const choice = prompt("Enter Rock, Paper or Scissor");
-
-    console.log(choice.toLowerCase());
-}
-
-//3. Create a funtion that plays the game round;
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = humanPlay();
-    computerSelection = computerPlay();
-    // Conditions for draw
-    if (playerSelection === "rock" && computerSelection === "rock") {
-        console.log("You tied");
-    } else if (playerSelection === "paper" && computerSelection === "paper") {
-        console.log("You tied");
-    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        console.log("You tied");
-    }
-    
-    // Conditions to check player wins
-    
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("Yay! Rock beats scissors! Player Wins");
-    }else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("Yay! Paper wraps rock! Player Wins");
-    }else if (playerSelection === "scissor" && computerSelection === "paper") {
-        console.log("Yay! Scissor cuts paper! Player Wins");
-    }
-
-    // Conditions to check computer wins
-
-    else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("Yay!Scissors cuts Paper! Computer Wins");
-    }else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("Yay! Paper wraps rock! Computer Wins");
-    }else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log("Yay! Rock beats scissors! Computer Wins");
-    }
-}
-
-playRound();
+};
