1463e4199a106bf669aac5ddc53a5256723d669f
Kevin-Code-Master <koluoch28@gmail.com>
2025-05-02 17:19:30 +0300

    Updating the RPS on a user interface branch

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
+//grab the buttons and the text inputs
+const enterBtn = document.getElementById("enter"); //Enter button
+const resetBtn = document.getElementById("reset"); //Reset button
+ //Input for the player name
+let playerNameInput = document.getElementById("playerNameInput");
+//Player name H1
+let playerName = document.getElementById("playerName");
+let roundsToPlay = document.getElementById("roundsToPlay");
+
+//Write a function to collect and display the player name
+//* When a player enters the name
+//* When a player enters the number of rounds
+//* When a player clicks enter button
+//* When the reset button is clicked should reset everything
+function enterPlayerName() {
+    enterBtn.addEventListener ("click", () =>{
+        if (playerNameInput.value && roundsToPlay.value > 0) {
+            playerName.textContent = playerNameInput.value;
+        }else {
+            playerName.textContent = "Player 1"
+        };
+    });
+};
+enterPlayerName();
+
+//Reset the name and the rounds to play
+resetBtn.addEventListener("click", () =>{
+    playerName.textContent = "Player 1";
+    playerNameInput.value = "";
+    roundsToPlay.value = 0;
+});
+//Grab the images using their ids
+//Computer Images
+const scissors = document.getElementById("scissors");
+const paper = document.getElementById("paper");
+const rock = document.getElementById("rock");
+
+//Player Images
+const scissors2 = document.getElementById("scissors2");
+const paper2 = document.getElementById("paper2");
+const rock2 = document.getElementById("rock2");
+
+
 // // 1. Create a computer selection using random selection
 function getComputerChoice() {
     const random = Math.floor(Math.random() * 3);
@@ -9,24 +52,31 @@ function getComputerChoice() {
         return "scissors";
     };
 };
-// // 2. Create human selection function using prompt
+// 2. Create human selection function using the buttons
 function getHumanChoice() {
-    const choice = prompt("Enter Rock, Paper or Scissor")
-    const str = choice.toLowerCase();
-    return str;
+    const rockBtn = document.getElementById("rockbtn");
+    const paperBtn = document.getElementById("paperbtn");
+    const scissorBtn = document.getElementById("scissorbtn");
+    
+    rockBtn.addEventListener("click", () => {
+        rock.style.display = "block";
+        paper2.style.display = "none";
+        scissors2.style.display = "none";
+    });
 };
+getHumanChoice()
 // // Score variables
-let humanScore = 0;
-let computerScore = 0;
-// Function to play the game
-function playGame() {
-     for (let i = 1; i <= 5; i++) {
-        computerSelection = getComputerChoice();
-        humanSelection = getHumanChoice();
-        playRound(computerSelection, humanSelection);
-     };
-};
-playGame();
+// let humanScore = 0;
+// let computerScore = 0;
+// // Function to play the game
+// function playGame() {
+//      for (let i = 1; i <= 5; i++) {
+//         computerSelection = getComputerChoice();
+//         humanSelection = getHumanChoice();
+//         playRound(computerSelection, humanSelection);
+//      };
+// };
+// playGame();
 
 // //3. Create a function to play one round of the game
 function playRound() {
@@ -66,3 +116,4 @@ function playRound() {
         console.log("It is a draw. Play again");
     }
 };
+
